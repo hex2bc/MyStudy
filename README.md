05b54b09e370c97f711a194e8979aea080b2d223
hex2bc <hex2bc@gmail.com>
2019-05-21 14:24:57 +0800

    hello world

---
+++ README.md
@@ -0,0 +1,2 @@
+平时工作学习中的DEMO程序，方便同步和管理。  
+目前分为java, python, android 三部分。
---
+++ java/src/net/socket/Main.java
@@ -0,0 +1,11 @@
+package net.socket;
+
+/**
+ * Created by hex2bc on 2019/5/21.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
